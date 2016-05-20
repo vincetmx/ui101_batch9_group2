4e730523146ef868e120a055772cedf15ce46888
Jie Zhou <jeffjiezhou@gmail.com>
2016-05-19 20:14:48 -0400

    login page

--- dashboard/app/scripts/controllers/login.js
+++ dashboard/app/scripts/controllers/login.js
@@ -1,4 +1,7 @@
-'use strict';
+//'use strict';
 // LOGIN CONTROLLER
 // Description: Define the following functionalities:
 // Making service calls to login a user
+
+var login = angular.module(dashboard, []);
+
