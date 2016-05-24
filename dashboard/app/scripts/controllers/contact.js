dbf11025d53872ac6d094bf6310aee7823c93a1d
wdmzscz <cjzcheng@yahoo.com>
2016-05-24 14:29:00 -0400

    cj

--- dashboard/app/scripts/controllers/contact.js
+++ dashboard/app/scripts/controllers/contact.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // CONTACT CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Contact" section for the form
--- dashboard/app/scripts/controllers/login.js
+++ dashboard/app/scripts/controllers/login.js
@@ -2,3 +2,22 @@
 // LOGIN CONTROLLER
 // Description: Define the following functionalities:
 // Making service calls to login a user
+App.controller("loginCtrl",function($scope,$http,$cookieStore,$location){
+	var user=function(username,password){
+		this.username=username;
+		this.password=password;
+	}
+	$scope.submit=function(){
+		var users=new user($scope.username,$scope.password);
+	$http.post('http://localhost:3000/api/login',users)
+	.success(function(data,status,headers.config){
+		if(data.authentication=="success"){
+			$cookieStore.put("username",$scope.username);
+			$location.path("/root/work");
+		}
+	}).error(function(data,status,headers,config){
+		$scope.errorMsg="Login is not correct,please try again.";
+	});
+		
+	}
+})
