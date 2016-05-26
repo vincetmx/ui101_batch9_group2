ad172d4d233243ae52888a7318538136a2661eca
Jie Zhou <jeffjiezhou@gmail.com>
2016-05-26 14:27:56 -0400

    constructure done

--- dashboard/app/scripts/app.js
+++ dashboard/app/scripts/app.js
@@ -15,12 +15,28 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('login', {
             url: '/login',
-            templateUrl: 'templates/login.html',
+            templateUrl: '../templates/login.html',
             controller: 'loginCtrl'
         })
         .state('root', {
             url: '/root',
-            templateUrl: 'templates/root.html'
+            templateUrl: '../templates/root.html'
             //controller: ''
+        })
+        .state('root.overview',{
+            url: '/overview',
+            templateUrl: '../templates/overview.html'
+        })
+        .state('root.work', {
+            url: '/work',
+            templateUrl: '../templates/work.html'
+        })
+        .state('root.producer', {
+            url: '/producer',
+            templateUrl: '../templates/producer.html'
+        })
+        .state('root.contact', {
+            url: '/contact',
+            templateUrl: '../templates/contact.html'
         })
 });
