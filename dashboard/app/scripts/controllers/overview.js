92fca1e0fb1ccca25c4b5768067fb662b273991b
Jie Zhou <jeffjiezhou@gmail.com>
2016-05-31 13:51:07 -0400

    overview page done

--- dashboard/app/scripts/controllers/overview.js
+++ dashboard/app/scripts/controllers/overview.js
@@ -1,4 +1,23 @@
-'use strict';
+//'use strict';
 // OVERVIEW CONTROLLER
 // Description: Define the following functionalities:
 // Make service call to retrieve user's information
+
+app.controller('overviewCtrl', ['$scope', '$http', '$cookies', 'AuthService',
+    function($scope, $http, $cookies, AuthService){
+        $http({
+            url: '/api/profile',
+            method: 'GET'
+        })
+            .success(function(data, status, header, config){
+                var file;
+                for(file in data.profiles) {
+                    console.log(data.profiles[file].username);
+                    if(data.profiles[file].username == AuthService.ReadCookie()) {
+                        $scope.overviewProfile = data.profiles[file];
+                        break;
+                    }
+                }
+            });
+    }
+]);
