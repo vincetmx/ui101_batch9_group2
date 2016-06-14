023dce07cd2ae0b238229a9d6948be7795d8beb2
Jie Zhou <jeffjiezhou@gmail.com>
2016-06-14 00:40:21 -0400

    contact information

--- dashboard/app/scripts/controllers/contact.js
+++ dashboard/app/scripts/controllers/contact.js
@@ -1,4 +1,21 @@
-'use strict';
+// 'use strict';
 // CONTACT CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Contact" section for the form
+
+app.controller('contactCtrl',['$scope', '$http',
+    function($scope, $http){
+        $http({
+            method: 'GET',
+            url: '/api/location',
+        })
+        .success(function(data, status, headers, config, statusText){
+            //console.log('Success');
+            $scope.states = data;
+            //console.log($scope.states);
+        })
+        .error(function(){
+            alert('$http call fails');
+        });
+    }
+]);
