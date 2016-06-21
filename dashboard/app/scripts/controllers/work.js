8e26705efbc7c020749214f2158a9868ebf9f8f9
Jie Zhou <jeffjiezhou@gmail.com>
2016-06-21 00:05:50 -0400

    edit modal is done

--- dashboard/app/scripts/controllers/work.js
+++ dashboard/app/scripts/controllers/work.js
@@ -3,9 +3,8 @@
 // Description: Define the following functionalities:
 // All the functionalities on the "Work" section to display items and filter them
 
-app.controller('workCtrl', ['$scope', '$http', '$filter', '$uibModal',
-    function($scope, $http, $filter, $uibModal){
-
+app.controller('workCtrl', ['$scope', '$http', '$filter', '$uibModal', '$log', '$rootScope',
+    function($scope, $http, $filter, $uibModal, $log, $rootScope){
         //
         $http({
             method: 'GET',
@@ -36,9 +35,9 @@ app.controller('workCtrl', ['$scope', '$http', '$filter', '$uibModal',
         // open modal
         $scope.workDelete = function ($index) {
             //alert($index);
-            var modalInstance = $uibModal.open({
+            var deleteModalInstance = $uibModal.open({
                 animation: true,
-                controller: 'ModalInstanceCtrl',
+                controller: 'deleteModalInstanceCtrl',
                 size: '',
                 keyboard: true,
                 templateUrl:'../templates/confirmBox.html',
@@ -49,33 +48,70 @@ app.controller('workCtrl', ['$scope', '$http', '$filter', '$uibModal',
                     },
                 }
             });
-
-
-            modalInstance.result.then(function () {
-                console.log($scope.workFile.works);
-
+            deleteModalInstance.result.then(function () {
+                //console.log($scope.workFile.works);
                 $scope.workFile.works.splice($index, 1);
             }, function () {
                 $log.info('Modal dismissed at: ' + new Date());
             });
         };
 
+        $scope.workEdit = function(work){
+            $rootScope.editWork = work;
+            var editModalInstance = $uibModal.open({
+                animation: true,
+                controller: 'editModalInstanceCtrl',
+                size: '',
+                keyboard: true,
+                templateUrl: '../templates/editDialog.html',
+                resolve: {
+
+                }
+            });
+            editModalInstance.result.then(function(){
+
+            }, function(){
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        }
+        //console.log($rootScope.editWork);
+        //$scope.editWorkImage = $rootScope.editWork.image;
+        //$scope.editWorkTitle = $rootScope.editWork.title;
+        //$scope.editWorkAuthor = $rootScope.editWork.author;
+
     }
 ]);
 
-app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, index) {
+app.controller('deleteModalInstanceCtrl', function ($scope, $uibModalInstance) {
 
     //$scope.items = items;
     //$scope.selected = {
     //    item: $scope.items[0]
     //};
-
     $scope.ok = function () {
-        //console.log(index);
-        $uibModalInstance.close('delete the ' + index + 'th item');
+        $uibModalInstance.close('delete the th item');
     };
 
     $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
+
+app.controller('editModalInstanceCtrl', function($scope, $uibModalInstance){
+    $scope.editWorkImage = $scope.editWork.image;
+    $scope.editWorkTitle = $scope.editWork.title;
+    $scope.editWorkAuthor = $scope.editWork.author;
+
+    $scope.submit = function(){
+        $scope.editWork.image = $scope.editWorkImage;
+        $scope.editWork.title = $scope.editWorkTitle;
+        $scope.editWork.author = $scope.editWorkAuthor;
+
+        $uibModalInstance.close('edit the th item');
+    };
+
+    $scope.cancel = function(){
+        //console.log($rootScope.editWork);
         $uibModalInstance.dismiss('cancel');
     };
 });
